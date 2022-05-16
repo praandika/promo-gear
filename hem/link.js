93716f119ff446b596777c5ff5ac3e2218b19cbf
Andika Pranayoga <praandikayoga@gmail.com>
2022-05-16 13:45:04 +0800

    update link js

--- hem/link.js
+++ hem/link.js
@@ -1,1 +1,6 @@
-document.getElementById("link").innerHTML = "link : " +window.location.pathname;
+let pathName = document.getElementById("link").innerHTML = "link : " +window.location.pathname;
+if (pathName == '/1/') {
+    document.getElementsByClassName("path").innerHTML = "Rini";
+} else {
+    document.getElementsByClassName("path").innerHTML = "Error";
+}
